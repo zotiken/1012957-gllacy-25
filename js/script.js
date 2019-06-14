1acc7bb6a619386a93b90a08a98bc6005a552bce
nikita <zotiken@gmail.com>
2019-06-14 20:10:07 +0300

    Основное задание: завершаем вёрстку личного проекта

--- js/script.js
+++ js/script.js
@@ -26,3 +26,64 @@ for (var i = 0; i < rad.length; i++) {
     }
     }
    }  
+// ------------------------------------------------------
+  var link = document.querySelector(".form-feedback-link");
+  
+  var popup = document.querySelector(".feedback-form");
+  var overlay = document.querySelector(".modal-overlay");
+  var close = popup.querySelector(".modal-closed");
+  
+  var form = popup.querySelector("form");
+  var name = popup.querySelector("[name=name]");
+  var email = popup.querySelector("[name=email]");
+  
+  var isStorageSupport = true;
+  var storage = "";
+
+  try {
+    storage = localStorage.getItem("name");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+  
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal");
+    overlay.classList.add("modal");
+    
+    if (storage) {
+      name.value = storage;
+      email.focus();
+    } else {
+      name.focus();
+    }
+  });
+  
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal");
+    popup.classList.remove("modal-error");
+  });
+  
+//   form.addEventListener("submit", function (evt) {
+//     if (!name.value || !email.value) {
+//       evt.preventDefault();
+//       popup.classList.remove("modal-error");
+//       popup.offsetWidth = popup.offsetWidth;
+//       popup.classList.add("modal-error");
+//     } else {
+//       if (isStorageSupport) {
+//         localStorage.setItem("name", name.value);
+//       }
+//     }
+//   });
+  
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal")) {
+        popup.classList.remove("modal");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
