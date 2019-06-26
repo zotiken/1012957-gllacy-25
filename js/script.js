16864dba3c91dd6d45f9bb10a3286e9d4b219416
nikita <zotiken@gmail.com>
2019-06-26 17:09:18 +0300

    финальная дороботка проекта с внесением исправлений

--- js/script.js
+++ js/script.js
@@ -1,89 +1,89 @@
 // "use strict"
-var rad = document.querySelectorAll( '.slider-toggle' );
+
+// -----------slider-----------------
+
+var rad = document.querySelectorAll('.slider-toggle');
 var img = document.querySelectorAll(".img-slider");
-var body = document.querySelector( "body");
+var body = document.querySelector("body");
+var title = document.querySelector(".poster-title");
 console.log(rad);
 console.log(img);
 console.log(body);
+
 function slider() {
-for (var i = 0; i < rad.length; i++) {
-   if (rad[i].checked) {
-       console.log(rad[i]);
-    img[i].style.opacity = '1';
-    if (i == 0) {
-        body.style.backgroundColor = '#849d8f';
-    }
-    if (i == 1) {
-        body.style.backgroundColor = '#8996a6';
-    }
-    if (i == 2) {
-        body.style.backgroundColor = '#9d8b84';
+    for (var i = 0; i < rad.length; i++) {
+        if (rad[i].checked) {
+            console.log(rad[i]);
+            img[i].style.opacity = '1';
+            if (i == 0) {
+                body.style.backgroundColor = '#849d8f';
+                title.innerHTML = "Крем-брюле и пломбир с малиновым джемом";
+            }
+            if (i == 1) {
+                body.style.backgroundColor = '#8996a6';
+                title.innerHTML = "Шоколадный пломбир и лимонный сорбет";
+            }
+            if (i == 2) {
+                body.style.backgroundColor = '#9d8b84';
+                title.innerHTML = "Пломбир с помадкой и клубничный щербет";
+            }
+        } else {
+            img[i].style.opacity = '0';
+        }
     }
 }
-   
-    else {
-        img[i].style.opacity = '0';
-    }
-    }
-   }  
-// ------------------------------------------------------
-  var link = document.querySelector(".form-feedback-link");
-  
-  var popup = document.querySelector(".feedback-form");
-  var overlay = document.querySelector(".modal-overlay");
-  var close = popup.querySelector(".modal-closed");
-  
-  var form = popup.querySelector("form");
-  var name = popup.querySelector("[name=name]");
-  var email = popup.querySelector("[name=email]");
-  
-  var isStorageSupport = true;
-  var storage = "";
 
-  try {
+// -----------------modal-----------------------------
+
+var link = document.querySelector(".form-feedback-link");
+
+var popup = document.querySelector(".feedback-form");
+var overlay = document.querySelector(".modal-overlay");
+var close = popup.querySelector(".modal-closed");
+
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
     storage = localStorage.getItem("name");
-  } catch (err) {
+} catch (err) {
     isStorageSupport = false;
-  }
-  
-  link.addEventListener("click", function (evt) {
+}
+
+link.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.add("modal");
     overlay.classList.add("modal");
-    
+
     if (storage) {
-      name.value = storage;
-      email.focus();
+        name.value = storage;
+        email.focus();
     } else {
-      name.focus();
+        name.focus();
     }
-  });
-  
-  close.addEventListener("click", function (evt) {
+});
+
+close.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.remove("modal");
-    popup.classList.remove("modal-error");
-  });
-  
-//   form.addEventListener("submit", function (evt) {
-//     if (!name.value || !email.value) {
-//       evt.preventDefault();
-//       popup.classList.remove("modal-error");
-//       popup.offsetWidth = popup.offsetWidth;
-//       popup.classList.add("modal-error");
-//     } else {
-//       if (isStorageSupport) {
-//         localStorage.setItem("name", name.value);
-//       }
-//     }
-//   });
-  
-  window.addEventListener("keydown", function (evt) {
+    overlay.classList.remove("modal");
+});
+
+overlay.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal");
+    overlay.classList.remove("modal");
+});
+
+window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal")) {
-        popup.classList.remove("modal");
-        popup.classList.remove("modal-error");
-      }
+        evt.preventDefault();
+        if (popup.classList.contains("modal")) {
+            popup.classList.remove("modal");
+        }
     }
-  });
+});
